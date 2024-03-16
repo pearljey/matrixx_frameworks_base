77f159faea42de78ecc7858c2963e3559ad8e95a
Pearljey The Legend <157608972+pearljey@users.noreply.github.com>
2024-03-17 02:29:40 +0800

    Update Build.java
    Solve the internal storage error for LG-G710

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1305,7 +1305,7 @@ public class Build {
      */
     public static boolean isBuildConsistent() {
         // Don't care on eng builds.  Incremental build may trigger false negative.
-        if (IS_ENG) return true;
+        /*if (IS_ENG) return true;
 
         if (IS_TREBLE_ENABLED) {
             // If we can run this code, the device should already pass AVB.
@@ -1340,7 +1340,7 @@ public class Build {
                         + " but vendor reported " + vendor);
                 return false;
             }
-        }
+        } */
 
         /* TODO: Figure out issue with checks failing
         if (!TextUtils.isEmpty(bootimage)) {
